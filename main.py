0a691fef41b084365a9ebe03a944221786c60115
Bartłomiej Śmietanka <bartek.smietanka@gmail.com>
2018-10-13 13:13:00 +0200

    Flask hello world

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    print("Hello World")
+    app.run()
