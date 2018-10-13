dfe488ea78ea9bd5766358f81ceeeb141c6be936
RegyN <250901@pw.edu.pl>
2018-10-13 17:40:48 +0200

    Zmiany w bazie

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
 import os
+from makebase import Base, Offer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return os.environ.get('DATABASE_URL')
+    database_url = os.environ.get('DATABASE_URL')
+    engine = create_engine(database_url)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+    return session.query(Offer).all()
 
 if __name__ == "__main__":
     app.run()
--- seedbase.py
+++ seedbase.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import datetime
+from datetime import datetime
+import time
+#import datetime as dtime
+import json
+import pprint
  
 from makebase import Offer, Base
  
@@ -10,7 +14,31 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
  
-# Insert an Ofer in the person table
-new_offer = Offer(name='new offer')
+# Insert an Offer in the person table
+nowDate = datetime(2012,3,3,10,10,10)
+
+with open("offer.json") as f:
+    content = f.read()
+parsed = json.loads(content)
+
+print(json.dumps(parsed, indent=4, sort_keys=True))
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(parsed)
+start_coord = parsed["start_location"]
+end_coord = parsed["end_location"]
+parsed.pop("start_location", None)
+parsed.pop("end_location", None)
+
+parsed["time_end"] =  time.strptime(parsed["time_end"],"%H:%M")
+parsed["time_start"] = time.strptime(parsed["time_start"],"%H:%M")
+parsed["start_lat"] = start_coord["lat"]
+parsed["start_long"] = start_coord["long"]
+parsed["end_lat"] = end_coord["lat"]
+parsed["end_long"] = end_coord["long"]
+
+
+new_offer = Offer(**parsed)
 session.add(new_offer)
 session.commit()
+
+print(session.query(Offer).first())
